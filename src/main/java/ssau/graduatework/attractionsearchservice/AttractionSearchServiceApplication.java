cb9706a8e210bb224d7980bfd733341db79de75f
Дарья Архипова <darkh0802@gmail.com>
2024-05-15 12:20:36 +0400

    Initial commit

---
+++ src/main/java/ssau/graduatework/attractionsearchservice/AttractionSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package ssau.graduatework.attractionsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttractionSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttractionSearchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ssau/graduatework/attractionsearchservice/AttractionSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ssau.graduatework.attractionsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttractionSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
