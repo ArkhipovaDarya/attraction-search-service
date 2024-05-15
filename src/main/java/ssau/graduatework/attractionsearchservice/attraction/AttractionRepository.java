c44599e546609607627b189e11da8ee9d6473597
Дарья Архипова <darkh0802@gmail.com>
2024-05-15 14:16:48 +0400

    Fixed problem with schema

--- src/main/java/ssau/graduatework/attractionsearchservice/attraction/AttractionRepository.java
+++ src/main/java/ssau/graduatework/attractionsearchservice/attraction/AttractionRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 @Repository
 public interface AttractionRepository extends JpaRepository<Attraction, Integer> {
-    @Query(value = "select information, rate from attraction where name = ?1", nativeQuery = true)
+    @Query(value = "select information, rate from public.attraction where name = ?1", nativeQuery = true)
     List<Object> getInformationAndRate(String attraction);
     Optional<Attraction> findAttractionById(Integer id);
 }
